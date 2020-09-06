60734fa3bab0f7d835661a1fd91de945ac14606d
Oya Junki <jkunk06@gmail.com>
2020-09-06 22:57:21 +0900

    first commit

---
+++ EtherTestSend/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace EtherTestSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_RecvStart = new System.Windows.Forms.Button();
+            this.tbx_RecvData = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btn_RecvStart
+            // 
+            this.btn_RecvStart.Location = new System.Drawing.Point(12, 12);
+            this.btn_RecvStart.Name = "btn_RecvStart";
+            this.btn_RecvStart.Size = new System.Drawing.Size(129, 23);
+            this.btn_RecvStart.TabIndex = 0;
+            this.btn_RecvStart.Text = "受信開始";
+            this.btn_RecvStart.UseVisualStyleBackColor = true;
+            this.btn_RecvStart.Click += new System.EventHandler(this.btn_RecvStart_Click);
+            // 
+            // tbx_RecvData
+            // 
+            this.tbx_RecvData.Location = new System.Drawing.Point(12, 92);
+            this.tbx_RecvData.Multiline = true;
+            this.tbx_RecvData.Name = "tbx_RecvData";
+            this.tbx_RecvData.Size = new System.Drawing.Size(414, 346);
+            this.tbx_RecvData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 450);
+            this.Controls.Add(this.tbx_RecvData);
+            this.Controls.Add(this.btn_RecvStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_RecvStart;
+        private System.Windows.Forms.TextBox tbx_RecvData;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ EtherTestSend/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace EtherTestSend
+{
+    public partial class Form1 : Form
+    {
+        //受信設定
+        private UdpClient udpClientRecv = null;
+        private IAsyncResult recvState = null;
+        //送信設定
+        private UdpClient udpClientSend = null;
+        private int remotePort = 30001;
+        private string remoteHost = "192.168.1.208";
+
+        private System.Threading.Timer timer;
+        private bool sendBtnFlag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_RecvStart_Click(object sender, EventArgs e)
+        {
+            if (sendBtnFlag)
+            {
+                timer.Change(Timeout.Infinite, Timeout.Infinite);
+                SendBtnAction(false, "送信開始");
+            }
+            else
+            {
+                SendBtnAction(true, "送信終了");
+
+                if (udpClientRecv == null)
+                {
+                    udpClientRecv = new UdpClient(30000);
+                }
+
+                timer = new System.Threading.Timer(TimerProc);
+                timer.Change(0, 1000);
+            }
+        }
+        private void TimerProc(object state)
+        {
+            if (recvState == null)
+            {
+                recvState = udpClientRecv.BeginReceive(RecvCallback, udpClientRecv);
+            }
+        }
+        private void RecvCallback(IAsyncResult ar)
+        {
+            recvState = null;
+            UdpClient udp = (UdpClient)ar.AsyncState;
+            IPEndPoint remoteEP = null;
+            byte[] recvBytes = udp.EndReceive(ar, ref remoteEP);
+            string recvText = "";
+            for(int i=0; i< recvBytes.Length; i++)
+            {
+                recvText += recvBytes[i];
+            }
+
+            tbx_RecvData.BeginInvoke(new Action<string>(RecvTextShow), recvText);
+
+            if(udpClientSend == null)
+            {
+                udpClientSend = new UdpClient();
+            }
+            udpClientSend.BeginSend(recvBytes, recvBytes.Length, remoteHost, remotePort, SendCallback, udpClientSend);
+        }
+        private void SendCallback(IAsyncResult ar)
+        {
+        }
+        private void RecvTextShow(string str)
+        {
+            tbx_RecvData.Text = str + "\r\n" + tbx_RecvData.Text;
+        }
+        private void SendBtnAction(bool flag, string text)
+        {
+            sendBtnFlag = flag;
+            btn_RecvStart.Text = text;
+        }
+    }
+}
